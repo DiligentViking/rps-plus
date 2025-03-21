69cd7954a71b3c59a1b3918797fd416ac3f64609
DiligentViking <acore-n@proton.me>
2025-03-21 10:55:03 -0500

    Optimize event listening and implement game stop

--- javascript.js
+++ javascript.js
@@ -11,9 +11,11 @@ function getComputerChoice() {
 }
 
 
-function playGame() {
-  let humanScore = 0
-  let computerScore = 0
+function playGame(winScore) {
+  let gameWon = false;
+
+  let humanScore = 0;
+  let computerScore = 0;
 
   function playRound(humanChoice, computerChoice) {
     let msg
@@ -52,40 +54,35 @@ function playGame() {
 
     // we update instead of add (which i could do by creating the p elems or something)
     const results = document.querySelector('#results');
-    document.querySelector('#youChoice').textContent = `You: ${humanChoice}`;
-    document.querySelector('#cpuChoice').textContent = `CPU: ${computerChoice}`;
+    document.querySelector('#youChoice').textContent = `You:\t${humanChoice}`;
+    document.querySelector('#cpuChoice').textContent = `CPU:\t${computerChoice}`;
     document.querySelector('#matchMsg').textContent = `${msg}`;
     document.querySelector('#score').textContent = `Score: You ${humanScore} :: CPU ${computerScore}`;
   }
 
-  function checkWinner(winScore) {
+  function checkWinner() {
     const winMsg = document.querySelector('#winMsg');
     if (humanScore >= winScore) {
       winMsg.textContent = 'THE HUMAN IS THE WINNER';
+      return true;
     } else if (computerScore >= winScore) {
       winMsg.textContent = 'THE COMPUTER IS THE WINNER';
+      return true;
     }
   }
 
-  // consider optimizing into one event listener
-  const rockBtn = document.querySelector('#rock');
-  rockBtn.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
-    checkWinner(5);
-  });  // a func inside a func?
-  const paperBtn = document.querySelector('#paper');
-  paperBtn.addEventListener('click', () => {
-    playRound('paper', getComputerChoice());
-    checkWinner(5);
-  })
-  const scissorBtn = document.querySelector('#scissor');
-  scissorBtn.addEventListener('click', () => {
-    playRound('scissor', getComputerChoice());
-    checkWinner(5);
+  const selection = document.querySelector('#selection');
+  selection.addEventListener('click', (event) => {
+    const choice = event.target.id;
+    const validChoices = ['rock', 'paper', 'scissor'];
+    if (validChoices.includes(choice)) {
+      playRound(choice, getComputerChoice());
+      if (checkWinner()) selection.remove();
+    }
   })
 
 }
 
 
 
-playGame()
+playGame(5)
